f8e582e80baf0aeb934625a9f38b682ec37acb81
JKurzer <jake.kurzer@gmail.com>
2024-09-02 01:57:32 -0400

    fix bug re: controller logic never proc'ing

--- Source/Bristlecone/Private/FCabling.cpp
+++ Source/Bristlecone/Private/FCabling.cpp
@@ -5,16 +5,17 @@
 using std::bitset;
 
 FCabling::FCabling()
-	: running(false) {
-
+	: running(false)
+{
 }
 
-FCabling::~FCabling() {
+FCabling::~FCabling()
+{
 }
 
 
-
-bool FCabling::Init() {
+bool FCabling::Init()
+{
 	UE_LOG(LogTemp, Display, TEXT("FCabling: Initializing Control Intercept thread"));
 
 	running = true;
@@ -22,7 +23,8 @@ bool FCabling::Init() {
 }
 
 
-void FCabling::Keyboard(IGameInputReading* reading, bool& sent, int seqNumber, uint64_t& priorReading, uint64_t& currentRead, const uint32_t sendHertzFactor)
+void FCabling::Keyboard(IGameInputReading* reading, bool& sent, int seqNumber, uint64_t& priorReading,
+                        uint64_t& currentRead, const uint32_t sendHertzFactor)
 {
 	uint32_t keyCount = reading->GetKeyCount();
 
@@ -32,7 +34,7 @@ void FCabling::Keyboard(IGameInputReading* reading, bool& sent, int seqNumber, u
 
 	double xMagnitude = 0.0;
 	double yMagnitude = 0.0;
-				
+
 	for (uint32_t i = 0; i < keyCount; i++)
 	{
 		// A
@@ -57,7 +59,7 @@ void FCabling::Keyboard(IGameInputReading* reading, bool& sent, int seqNumber, u
 		}
 	}
 
-	if (!sent)
+	if (xMagnitude != 0 || yMagnitude != 0)
 	{
 		FCableInputPacker boxing;
 		boxing.lx = (uint32_t)boxing.IntegerizedStick(xMagnitude);
@@ -78,48 +80,46 @@ void FCabling::Keyboard(IGameInputReading* reading, bool& sent, int seqNumber, u
 		}
 		priorReading = currentRead;
 	}
-	
 }
 
-void FCabling::GamePad(bool& sent, int seqNumber, uint64_t& priorReading, uint64_t& currentRead, const uint32_t sendHertzFactor, GameInputGamepadState state)
+void FCabling::GamePad(bool& sent, int seqNumber, uint64_t& priorReading, uint64_t& currentRead,
+                       const uint32_t sendHertzFactor, GameInputGamepadState state)
 {
-	if (!sent)
-	{
-		FCableInputPacker boxing;
-		//very fun story. unless you explicitly import and use std::bitset
-		//the wrong thing happens here. I'm not going to speculate on why, because
-		//I don't think I can do so without swearing extensively.
-		boxing.lx = (uint32_t)boxing.IntegerizedStick(state.leftThumbstickX);
-		boxing.ly = (uint32_t)boxing.IntegerizedStick(state.leftThumbstickY);
-		boxing.rx = (uint32_t)boxing.IntegerizedStick(state.rightThumbstickX);
-		boxing.ry = (uint32_t)boxing.IntegerizedStick(state.rightThumbstickY);
-		boxing.buttons = (uint32_t)state.buttons; //strikingly, there's no paddle field.
-		boxing.buttons.set(12, (state.leftTrigger > 0.55)); //check the bitfield.
-		boxing.buttons.set(13, (state.rightTrigger > 0.55));
-		boxing.events = 0;
-		currentRead = boxing.PackImpl();
+	FCableInputPacker boxing;
+	//very fun story. unless you explicitly import and use std::bitset
+	//the wrong thing happens here. I'm not going to speculate on why, because
+	//I don't think I can do so without swearing extensively.
+	boxing.lx = (uint32_t)boxing.IntegerizedStick(state.leftThumbstickX);
+	boxing.ly = (uint32_t)boxing.IntegerizedStick(state.leftThumbstickY);
+	boxing.rx = (uint32_t)boxing.IntegerizedStick(state.rightThumbstickX);
+	boxing.ry = (uint32_t)boxing.IntegerizedStick(state.rightThumbstickY);
+	boxing.buttons = (uint32_t)state.buttons; //strikingly, there's no paddle field.
+	boxing.buttons.set(12, (state.leftTrigger > 0.55)); //check the bitfield.
+	boxing.buttons.set(13, (state.rightTrigger > 0.55));
+	boxing.events = 0;
+	currentRead = boxing.PackImpl();
 
-		//because we deadzone and integerize, we actually have a pretty good idea
-		//of when input actually changes. 2048 positions for the stick along each axis
-		//actually looks like it's enough to give us precise movement while still
-		//excising some amount of jitter. Because we always round down, you have to move
-		//fully to a new position and this seems to be a larger delta than the average
-		//heart-rate jitter or control noise.
-		//TODO: this line might actually be wrong. I just redid the math, and it looks right but...
-		if ((seqNumber % sendHertzFactor) == 0 || (currentRead != priorReading))
-		{
-			//push to both queues.
-			this->CabledThreadControlQueue.Get()->Enqueue(currentRead);
-			this->GameThreadControlQueue.Get()->Enqueue(currentRead);
-			WakeTransmitThread->Trigger();
-			sent = true;
-		}
-		priorReading = currentRead;
+	//because we deadzone and integerize, we actually have a pretty good idea
+	//of when input actually changes. 2048 positions for the stick along each axis
+	//actually looks like it's enough to give us precise movement while still
+	//excising some amount of jitter. Because we always round down, you have to move
+	//fully to a new position and this seems to be a larger delta than the average
+	//heart-rate jitter or control noise.
+	//TODO: this line might actually be wrong. I just redid the math, and it looks right but...
+	if ((seqNumber % sendHertzFactor) == 0 || (currentRead != priorReading))
+	{
+		//push to both queues.
+		this->CabledThreadControlQueue.Get()->Enqueue(currentRead);
+		this->GameThreadControlQueue.Get()->Enqueue(currentRead);
+		WakeTransmitThread->Trigger();
+		sent = true;
 	}
+	priorReading = currentRead;
 }
 
 //this is based directly on the gameinput sample code.
-uint32 FCabling::Run() {
+uint32 FCabling::Run()
+{
 	IGameInput* g_gameInput = nullptr;
 	HRESULT gameInputSpunUp = GameInputCreate(&g_gameInput);
 	IGameInputDevice* g_gamepad = nullptr;
@@ -136,12 +136,11 @@ uint32 FCabling::Run() {
 	uint32_t lsbTime = NarrowClock::getSlicedMicrosecondNow();
 	constexpr uint32_t sampleHertz = TheCone::CablingSampleHertz;
 	constexpr uint32_t sendHertz = TheCone::BristleconeSendHertz;
-	constexpr uint32_t sendHertzFactor = sampleHertz/sendHertz; 
+	constexpr uint32_t sendHertzFactor = sampleHertz / sendHertz;
 	constexpr uint32_t Period = 1000000 / sampleHertz; //swap to microseconds. standardizing.
 
 
-	
-	constexpr auto HalfStep = std::chrono::microseconds(Period/2);
+	constexpr auto HalfStep = std::chrono::microseconds(Period / 2);
 	//We're using the GameInput lib.
 	//https://learn.microsoft.com/en-us/gaming/gdk/_content/gc/input/overviews/input-overview
 	//https://learn.microsoft.com/en-us/gaming/gdk/_content/gc/input/advanced/input-keyboard-mouse will be fun
@@ -165,51 +164,43 @@ uint32 FCabling::Run() {
 	{
 		if ((lastPollTime + Period) <= lsbTime)
 		{
-			
 			lastPollTime = lsbTime;
-			// I dunno, but I'm gonna try and put some keyboard detection code in here and see what happens *shrugs*
-			if (g_gameInput && SUCCEEDED(g_gameInput->GetCurrentReading(GameInputKindKeyboard, nullptr, &reading)))
+			if (!sent && g_gameInput &&
+				SUCCEEDED(g_gameInput->GetCurrentReading(GameInputKindKeyboard, nullptr, &reading)))
 			{
-
+				//if we don't have a WASD input, we don't send, and we'll check the controller next.
 				Keyboard(reading, sent, seqNumber, priorReading, currentRead, sendHertzFactor);
 				reading->Release();
 			}
-			else if (g_gameInput && SUCCEEDED(g_gameInput->GetCurrentReading(GameInputKindGamepad, g_gamepad, &reading)))
+			else if (!sent && g_gameInput &&
+				SUCCEEDED(g_gameInput->GetCurrentReading(GameInputKindGamepad, g_gamepad, &reading)))
 			{
 				// If no device has been assigned to g_gamepad yet, set it
 				// to the first device we receive input from. (This must be
 				// the one the player is using because it's generating input.)
-				if (!g_gamepad) reading->GetDevice(&g_gamepad);
+				if (!g_gamepad)
+				{
+					reading->GetDevice(&g_gamepad);
+				}
 				// Retrieve the fixed-format gamepad state from the reading.
 				GameInputGamepadState state;
-				//This is a struct of form:
-				//GameInputGamepadButtons buttons
-				//float leftTrigger
-				//float rightTrigger
-				//float leftThumbstickX
-				//float leftThumbstickY
-				//float rightThumbstickX
-				//float rightThumbstickY
-				//And we'll integerize the sticks, convert the triggers into thresholded buttons
-				//then repack the buttons and ship.
-
 				reading->GetGamepadState(&state);
 				reading->Release();
 
 				GamePad(sent, seqNumber, priorReading, currentRead, sendHertzFactor, state);
-
 			}
 			else if (g_gamepad != nullptr)
 			{
 				g_gamepad->Release();
 				g_gamepad = nullptr;
 			}
-			
+
 
 			if ((seqNumber % sampleHertz) == 0)
 			{
 				long long now = std::chrono::steady_clock::now().time_since_epoch().count();
-				UE_LOG(LogTemp, Display, TEXT("Cabling hertz cycled: %lld against lsb %lld with last poll as %lld"), (now), lsbTime, lastPollTime);
+				UE_LOG(LogTemp, Display, TEXT("Cabling hertz cycled: %lld against lsb %lld with last poll as %lld"),
+				       (now), lsbTime, lastPollTime);
 			}
 
 			if ((seqNumber % sendHertzFactor) == 0)
@@ -224,9 +215,9 @@ uint32 FCabling::Run() {
 
 		//modified to ensure we don't oversleep. we busy cycle if we're less than 1.1 ms out.
 		lsbTime = NarrowClock::getSlicedMicrosecondNow();
-		if(lsbTime <=  (lastPollTime + Period) - ( 1.1* (HalfStep).count()))
+		if (lsbTime <= (lastPollTime + Period) - (1.1 * (HalfStep).count()))
 		{
-			std::this_thread::sleep_for(HalfStep); 
+			std::this_thread::sleep_for(HalfStep);
 			lsbTime = NarrowClock::getSlicedMicrosecondNow();
 		}
 		if (lsbTime < lastPollTime)
@@ -234,20 +225,28 @@ uint32 FCabling::Run() {
 			lastPollTime = lsbTime;
 		}
 	}
-	if (g_gamepad) g_gamepad->Release();
-	if (g_gameInput) g_gameInput->Release();
+	if (g_gamepad)
+	{
+		g_gamepad->Release();
+	}
+	if (g_gameInput)
+	{
+		g_gameInput->Release();
+	}
 	return 0;
 }
 
-void FCabling::Exit() {
+void FCabling::Exit()
+{
 	Cleanup();
 }
 
-void FCabling::Stop() {
+void FCabling::Stop()
+{
 	Cleanup();
 }
 
-void FCabling::Cleanup() {
+void FCabling::Cleanup()
+{
 	running = false;
 }
-
